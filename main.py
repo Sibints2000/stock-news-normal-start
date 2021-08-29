f3de3e04631e77619ded81f6202944d8a2b14233
Sibints2000 <sibints2000@gmail.com>
2021-08-29 19:21:02 +0530

    Sixtieth Commit

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ print(three_articles)
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
 
-[f"Headlines: {article title}. \nBrief: {article description}" for article in three_articles]
+[f"Headlines: {article['title']}. \nBrief: {article ['description']}" for article in three_articles]
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 
