99c9ce13bab7f32bc25746b1408c0ef3fbcf07c1
Sibints2000 <sibints2000@gmail.com>
2021-08-29 18:32:39 +0530

    Fifteenth Commit

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
-
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
 
 #TODO 2. - Get the day before yesterday's closing stock price
 
