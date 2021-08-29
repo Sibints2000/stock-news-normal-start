23eb4cfe2b805298e0475b1bd321a4c3ce772885
Sibints2000 <sibints2000@gmail.com>
2021-08-29 19:28:10 +0530

    Sixty Third Commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "GC85PQMYCTDMCUQ3"
 NEWS_API_KEY = "b7f230a1fd1a40f2b7974776cfe7f30b"
+TWILIO_SID = "AC88a8c9dc2194c9afa111d4f97f50afae"
 
     # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
