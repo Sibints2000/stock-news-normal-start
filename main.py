453999439293093c17251625757ae5c84c5be8cc
Sibints2000 <sibints2000@gmail.com>
2021-08-29 19:18:08 +0530

    Fifty Eighth Commit

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ print(three_articles)
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
+"Headlines: {article title}. \nBrief: {article description}"
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 
