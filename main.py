b68dc1ca2db56ed79709a283a23ae2f067c8677b
Sibints2000 <sibints2000@gmail.com>
2021-08-29 18:41:30 +0530

    Thirtieth Commit

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ print(difference)
 #TODO 4. - Work out the percentage difference in price between closing price yesterday
 # and closing price the day before yesterday.
 
-diffe_percent = difference / yesterday_closing_price
+diff_percent = (difference / yesterday_closing_price) * 100
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
