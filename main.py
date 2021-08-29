2a30ff06caa36b8c47d24dd45b9927106c607390
Sibints2000 <sibints2000@gmail.com>
2021-08-29 19:14:54 +0530

    Fifty Fourth Commit

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ print(three_articles)
 
 
 # STEP 3: Use twilio.com/docs/sms/quickstart/python
-#to send a separate message with each article's title and description to your phone number.
+# to send a separate message with each article's title and description to your phone number.
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
