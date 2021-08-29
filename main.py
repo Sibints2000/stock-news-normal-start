088641b0adacc80d9058ccccec11d72d093052df
Sibints2000 <sibints2000@gmail.com>
2021-08-29 18:13:34 +0530

    Fourth Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API = "GC85PQMYCTDMCUQ3"
+STOCK_API_KEY = "GC85PQMYCTDMCUQ3"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
